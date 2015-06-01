9d2f8306594ea2cf8c99ed74028154b51ffe6758
pcruzn <pablo.cruz@gmail.com>
2015-06-01 20:17:59 -0300

    initial

---
+++ Tests/src/cl/inacap/unidad1/activity/test/UsuarioTest.java
@@ -0,0 +1,15 @@
+package cl.inacap.unidad1.activity.test;
+
+import junit.framework.TestCase;
+
+public class UsuarioTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testValidarLogin() {
+		fail("Not yet implemented");
+	}
+
+}
